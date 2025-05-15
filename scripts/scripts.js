e8668e4796bf2198e2db864b480bb70e6abe14bc
Chanchingyan <chan0588@algonquinlive.com>
2025-05-15 11:50:07 -0400

    update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -28,3 +28,31 @@ console.log(ourClassNum);
 
 //const and let cannot be redeclared (or recreated)
 //let numStudent = 25; (this will also give you an error)
+
+/************************** 
+    Javascript datatypes
+***************************/
+
+//storing a value of true or false is called a boolean
+let doorClosed = true;
+console.log(doorClosed);
+
+//you can create an empty variable, which will return undefined
+let whiteboardContent;
+console.log(whiteboardContent) ;//should give you undefined 
+
+//you can use the keyword "null" (which means nothing)
+let classKids = null;
+
+//number - can be written without quotes (computer can recognize them)
+//string - is text that must be written in quotes
+
+//difference between == and ==
+const numLights = 12; //this variable is storing a number
+const numChair = "12"; //this variable is storing a piece of text
+
+console.log(numbers == numChairs); //gives you ture
+console.log(numLights === numChairs);//gives you false
+
+//two eqals == checks if they are the same value
+//three equals === checks if they are the same value and same datatype
